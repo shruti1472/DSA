053ea95e3619150b6cef4576626a6d5366b9fe8d
shruti1472 <shrutijsk7@gmail.com>
2025-12-21 13:46:12 +0530

    add linked list fundamental and question

---
+++ linkedList/usestruct.cpp
@@ -0,0 +1,28 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node{
+public:
+    int data;
+    Node* next;
+    
+public:
+    Node(int data1,Node* next1){
+        data=data1;
+        next=next1;
+    }
+};
+
+int main() {
+    // Write C++ code here
+vector<int>arr={1,2,3,4};
+Node* z=new Node(arr[0],nullptr);               //creating pointer
+cout<<z<<endl;              
+cout<<z->data<<endl;      //printing data part of the pointer
+Node y= Node(arr[0],nullptr);   //creating a object
+cout<<y.data<<endl;    //will print 10
+cout<<y.next<<endl;
+
+}
